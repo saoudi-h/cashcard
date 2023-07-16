f146efe0cf888ba197c7c10719b761567540c160
Hakim Saoudi <saoudihakim@gmai.com>
2023-07-16 21:24:11 +0200

    add repository crud and dependency H2

--- src/main/java/com/saoudi/cashcard/CashCardController.java
+++ src/main/java/com/saoudi/cashcard/CashCardController.java
@@ -5,19 +5,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
 
+    private CashCardRepository cashCardRepository;
+    public CashCardController(CashCardRepository cashCardRepository){
+        this.cashCardRepository = cashCardRepository;
+    }
+
     @GetMapping("/{requestedId}")
     public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-        if (requestedId.equals(99L)) {
-            CashCard cashCard = new CashCard(99L, 123.45);
-            return ResponseEntity.ok(cashCard);
-        } else {
+
+        Optional<CashCard> optionalCashCard = cashCardRepository.findById(requestedId);
+        if(optionalCashCard.isPresent()){
+            return ResponseEntity.ok(optionalCashCard.get());
+        }else{
             return ResponseEntity.notFound().build();
         }
     }
-
 }

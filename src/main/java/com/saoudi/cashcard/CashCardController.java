a403c643ed365e66b4a1a36177bf6278427c095b
Hakim Saoudi <saoudihakim@gmai.com>
2023-07-16 12:46:44 +0200

    add CashCardController

---
+++ src/main/java/com/saoudi/cashcard/CashCardController.java
@@ -0,0 +1,19 @@
+package com.saoudi.cashcard;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cashcards")
+public class CashCardController {
+
+    @GetMapping("/{requestedId}")
+    public ResponseEntity<String> findById(){
+        return ResponseEntity.ok("{}");
+    }
+
+}

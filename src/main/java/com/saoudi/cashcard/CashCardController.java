1ca021929c45be4e6b1bf795c20da4cf0593e499
Hakim Saoudi <saoudihakim@gmail.com>
2023-09-19 15:12:44 +0200

    update CashCardController

--- src/main/java/com/saoudi/cashcard/CashCardController.java
+++ src/main/java/com/saoudi/cashcard/CashCardController.java
@@ -1,10 +1,8 @@
 package com.saoudi.cashcard;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Optional;
 
 @RestController
@@ -18,12 +16,17 @@ public class CashCardController {
 
     @GetMapping("/{requestedId}")
     public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-
-        Optional<CashCard> optionalCashCard = cashCardRepository.findById(requestedId);
-        if(optionalCashCard.isPresent()){
-            return ResponseEntity.ok(optionalCashCard.get());
-        }else{
+        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+        if (cashCardOptional.isPresent()) {
+            return ResponseEntity.ok(cashCardOptional.get());
+        } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PostMapping
+    public ResponseEntity<CashCard> createCashCard() {
+
+        return null;
     }
 }
